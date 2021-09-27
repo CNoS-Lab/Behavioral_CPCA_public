3de41b534d023529fca6031f18c3cc526156f492
abhijitchinchani <42435648+abhijitchinchani@users.noreply.github.com>
2021-09-27 11:52:39 -0700

    Add files via upload

---
+++ get_CAPS_LSHS_item_names.m
@@ -0,0 +1,56 @@
+function Glabel = get_CAPS_LSHS_item_names()
+
+Glabel{1}{1} = 'Notice loud sounds';
+Glabel{1}{2} = 'Presence of being';
+Glabel{1}{3} = 'Hear thoughts repeatedly';
+Glabel{1}{4} = 'See lights or colors';
+Glabel{1}{5} = 'burning sensations';
+Glabel{1}{6} = 'hear noises or sounds';
+Glabel{1}{7} = 'hear own thoughts';
+Glabel{1}{8} = 'Detect smells';
+Glabel{1}{9} = 'Sensation of change in body';
+Glabel{1}{10} = 'Sensation that limbs are not connected';
+Glabel{1}{11} = 'Hear voices commenting';
+Glabel{1}{12} = 'Feel someone is touching';
+Glabel{1}{13} = 'Hear voices saying words';
+Glabel{1}{14} = 'Experience unexplained tastes';
+Glabel{1}{15} = 'Sensory overload';
+Glabel{1}{16} = 'Find that sounds are distorte';
+Glabel{1}{17} = 'Difficulty distinguishing sensations';
+Glabel{1}{18} = 'Smell unusually strong odours';
+Glabel{1}{19} = 'Find change in appearance';
+Glabel{1}{20} = 'Sensitive skin';
+Glabel{1}{21} = 'Think foods taste stronger';
+Glabel{1}{22} = 'Think face seems unusual ';
+Glabel{1}{23} = 'Colours seem brighter';
+Glabel{1}{24} = 'Feeling of being uplifted';
+Glabel{1}{25} = 'Find smells unusual';
+Glabel{1}{26} = 'Everyday things look abnormal';
+Glabel{1}{27} = 'Experience drastic time changes';
+Glabel{1}{28} = 'Hear two or more unexplained voices';
+Glabel{1}{29} = 'Notice smells or odours';
+Glabel{1}{30} = 'Notice unusual taste';
+Glabel{1}{31} = 'See things people cannot';
+Glabel{1}{32} = 'Hear sounds or music';
+
+Glabel{2}{1} = 'Passing thought frightens me';
+Glabel{2}{2} = 'Thoughts seem real';
+Glabel{2}{3} = 'Unrelated thoughts creep into mind';
+Glabel{2}{4} = 'Experience hearing voices';
+Glabel{2}{5} = 'Hear clear sounds in daydreams';
+Glabel{2}{6} = 'Life-like persons in daydream';
+Glabel{2}{7} = 'Hear clear tunes in daydreams';
+Glabel{2}{8} = 'Hear thoughts aloud';
+Glabel{2}{9} = 'Troubled by voices';
+Glabel{2}{10} = 'Seeing faces when none was there';
+Glabel{2}{11} = 'Experience voices and visions while sleepy';
+Glabel{2}{12} = 'Sensation of floating while sleepy';
+Glabel{2}{13} = 'Feeling of presence of someone deceased';
+Glabel{2}{14} = 'Smell odour when none was there';
+Glabel{2}{15} = 'Feel being touched';
+Glabel{2}{16} = 'Seen animals and things';
+
+
+Glabel{3} = [Glabel{1}, Glabel{2}];
+
+end
